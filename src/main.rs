1e428d8723555afef2d4410be86bc70e8e9025b3
Kevin Johnson <kevin@coolkev.com>
2025-03-16 12:20:22 -0400

    Good-Looking World

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use actix_web::{App, HttpResponse, HttpServer, Responder, get};
 
 #[get("/")]
 async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello, Beautiful World!")
+    HttpResponse::Ok().body("Hello, Good-Looking World!")
 }
 
 #[actix_web::main]
