9c0f2a25dabda127ce4c40efd2dbd587edcfe5e4
Kevin Johnson <kevin@coolkev.com>
2025-03-19 23:33:40 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use actix_web::{App, HttpResponse, HttpServer, Responder, get};
 
 #[get("/")]
 async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello, eeeeeeeeeeek World!")
+    HttpResponse::Ok().body("Hello, feeeeeeeeeeek World!")
 }
 
 #[actix_web::main]
