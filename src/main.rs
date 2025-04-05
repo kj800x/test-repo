0fe62a8dac968a43650ee6d80563ac39d92266aa
Kevin Johnson <kevin@coolkev.com>
2025-04-05 17:02:22 -0400

    main main push

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use actix_web::{App, HttpResponse, HttpServer, Responder, get};
 
 #[get("/")]
 async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello, main push World!")
+    HttpResponse::Ok().body("Hello, main main push World!")
 }
 
 #[actix_web::main]
