46e2ef4a8e389dbc297e22fb2dfe2b7f39f437e8
Kevin Johnson <kevin@coolkev.com>
2025-04-05 17:11:31 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use actix_web::{App, HttpResponse, HttpServer, Responder, get};
 
 #[get("/")]
 async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello, branc branch test World!")
+    HttpResponse::Ok().body("Hello, branc branch testddd World!")
 }
 
 #[actix_web::main]
