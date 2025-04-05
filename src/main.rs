399b0307bfa87f3540773b4ce425475f3f3f14ca
Kevin Johnson <kevin@coolkev.com>
2025-04-05 16:07:05 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use actix_web::{App, HttpResponse, HttpServer, Responder, get};
 
 #[get("/")]
 async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("BRANCH TEST!!")
+    HttpResponse::Ok().body("BRANCH TEST push!!")
 }
 
 #[actix_web::main]
