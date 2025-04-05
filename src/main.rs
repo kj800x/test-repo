7796b3fb1ace91ac2a00164df5b77d2cbd7446cf
Kevin Johnson <kevin@coolkev.com>
2025-04-05 15:22:54 -0400

    BRANCH TEST

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use actix_web::{App, HttpResponse, HttpServer, Responder, get};
 
 #[get("/")]
 async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello, heyyydddy World!")
+    HttpResponse::Ok().body("BRANCH TEST!!")
 }
 
 #[actix_web::main]
