0c53fe318130dac785ee9885b9f0c59eb597ed1f
Kevin Johnson <kevin@coolkev.com>
2025-04-08 02:13:46 -0400

    Reverse order

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 fn main() {
     println!("Hello, world!");
     std::thread::sleep(std::time::Duration::from_secs(1));
-    println!("1...");
-    std::thread::sleep(std::time::Duration::from_secs(1));
-    println!("2...");
-    std::thread::sleep(std::time::Duration::from_secs(1));
-    println!("3...");
+    println!("5...");
     std::thread::sleep(std::time::Duration::from_secs(1));
     println!("4...");
     std::thread::sleep(std::time::Duration::from_secs(1));
-    println!("5, done!");
+    println!("3...");
+    std::thread::sleep(std::time::Duration::from_secs(1));
+    println!("2...");
+    std::thread::sleep(std::time::Duration::from_secs(1));
+    println!("1, done!");
 }
