f706add0a1cd1e08acfd0272d086d6dca8ad50ab
Kevin Johnson <kevin@coolkev.com>
2025-10-05 01:58:27 -0400

    Print env vars on startup

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,11 @@ async fn hello() -> impl Responder {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     println!("Server starting at http://0.0.0.0:8080");
-
     println!("Hello World!");
+
+    for (env, value) in std::env::vars() {
+        println!("{}: {}", env, value);
+    }
 
     HttpServer::new(|| App::new().service(hello))
         .bind(("0.0.0.0", 8080))?
