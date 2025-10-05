e58b123d2f7be93cffaf3c389eb1e413bd2aa18b
Kevin Johnson <kevin@coolkev.com>
2025-10-05 00:50:24 -0400

    Commit alt

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,14 @@ use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 
 #[get("/")]
 async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Lets gooo!!")
+    HttpResponse::Ok().body("Lets gooo (to the upside down)!!")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     println!("Server starting at http://0.0.0.0:8080");
 
-    println!("Hello World!");
+    println!("Hello alternative World!");
 
     HttpServer::new(|| App::new().service(hello))
         .bind(("0.0.0.0", 8080))?
