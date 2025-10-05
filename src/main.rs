11939b38f83b6c2c971e7020a639d28587bf787d
Kevin Johnson <kevin@coolkev.com>
2025-10-05 00:49:39 -0400

    Make a change

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
-use actix_web::{App, HttpResponse, HttpServer, Responder, get};
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 
 #[get("/")]
 async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("hopefully this works!!")
+    HttpResponse::Ok().body("Lets gooo!!")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     println!("Server starting at http://0.0.0.0:8080");
-    
-    println!("Hi Migrations!");
+
+    println!("Hello World!");
 
     HttpServer::new(|| App::new().service(hello))
         .bind(("0.0.0.0", 8080))?
