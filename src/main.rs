498048cc5aacb5f4aa00a3bf1eb9471cfd9ab9a8
Kevin Johnson <kevin@coolkev.com>
2025-11-01 21:42:56 -0400

    Update hello function to list environment variables
    Modify hello function to return environment variables.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,14 @@ use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 
 #[get("/")]
 async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Lets gooo!!")
+    let mut vars: Vec<String> = std::env::vars()
+        .map(|(k, v)| format!("{}={}", k, v))
+        .collect();
+    vars.sort();
+
+    HttpResponse::Ok()
+        .content_type("text/plain; charset=utf-8")
+        .body(format!("Hello world\n\n{}", vars.join("\n")))
 }
 
 #[actix_web::main]
